e3b69e5636ce27fc2e486adf773bdd2dce01319d
NathanG-Wyca <nathan.grauer@wyca.fr>
2023-09-07 10:51:48 +0200

    working cfclient

--- setup-crazyflie.sh
+++ setup-crazyflie.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
+
 cd /workspaces/esp-monitor/crazyflie-lib-python && pip3 install -r requirements.txt && pip3 install -e .
 cd /workspaces/esp-monitor/crazyflie-clients-python && pip3 install -e .
